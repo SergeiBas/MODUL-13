02bdba3f18e887a0ca20839142e06dd3f91aafba
SergeiBas <sergei.bas06@gmail.com>
2023-05-08 00:19:17 +0300

    one

---
+++ src/main/java/Constants.java
@@ -0,0 +1,11 @@
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.net.http.HttpClient;
+
+public interface Constants {
+
+    String USER_URL = "https://jsonplaceholder.typicode.com/users";
+    Gson GSON = new GsonBuilder().setPrettyPrinting().create();
+    HttpClient CLIENT = HttpClient.newHttpClient();
+}
